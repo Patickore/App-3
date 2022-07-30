e3a8d7dd7cd8498626e6e482591def7cd3de31c9
Patrick <patri@LAPTOP-Q3T1BCTA>
2022-07-30 17:36:16 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace App_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float num1 = 25.85F;
+            double num2 = 25,85;
+            decimal num3 = 25.85M;
+            Console.WriteLine("{0} {1} {2}", num1, num2, num3);
+        }
+    }
+}
